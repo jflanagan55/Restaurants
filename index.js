b531553304e409f8c4ebd112be8fa21f8232e86e
Justin Flanagan <jflanagan5555@gmail.com>
2022-06-05 00:35:44 -0400

    need to add form validation

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const bcrypt = require("bcrypt");
 const session = require("express-session");
 const connectMongo = require("connect-mongo");
 const MongoStore = require("connect-mongo");
+const flash = require('connect-flash');
 
 const cuisine = ["", "Italian", "Mexican", "Greek", "Chinese", "Japanese", "Indian", "Thai", "American", "Spanish", "French", "Other"]
 const states = ["", "AK", "AL", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MP", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UM", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY"]
@@ -38,9 +39,8 @@ app.use(
     store: MongoStore.create({ mongoUrl: dbString }),
   })
 );
-// app.use(function(req,res,next){
-//     res.locals.id = req.session.user_id;
-// })
+app.use(flash());
+
 const requireAuth = (req, res, next) => {
   if (!req.session.user_id) {
     console.log("there is not id")
@@ -54,11 +54,26 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  res.render("register");
+  res.render("register",{messages: req.flash('error')});
 });
 app.post("/register", async (req, res) => {
-  const { username, password, email } = req.body;
-   const hashedPw = await bcrypt.hash(password, 12,)
+  const { username, password, confirmPassword, email } = req.body;
+  const usernameExists = await User.findOne({username});
+  if(usernameExists){
+    req.flash('error', 'Username is already in use')
+    return res.redirect('/register')
+  }
+  const emailExists = await User.findOne({email});
+  if(emailExists){
+    req.flash('error', 'Email is already in use')
+    return res.redirect('/register')
+  }
+  if(password !== confirmPassword){
+    req.flash('error', 'Passwords do not match')
+    return res.redirect('/register')
+  }
+  
+const hashedPw = await bcrypt.hash(password, 12,)
  
 const user = new User({ 
     username, 
@@ -67,36 +82,30 @@ const user = new User({
 await user.save();
 res.redirect("/login");
 });
+
+
 app.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login",{messages: req.flash('error')});
 });
 app.post("/login", async (req, res) => {
     const { username, password } = req.body;
     const validUser = await User.findOne({username})
+    if(!validUser){
+      req.flash("error", 'Incorrect Username or Password')
+     return res.redirect('login')
+    }
     const validate = await bcrypt.compare(password, validUser.password)
 
     if(!validate){
-      console.log("no sir")
-      return
+     req.flash("error", 'Incorrect Username or Password')
+     return res.redirect('login')
     }
     req.session.user_id = validUser._id;
     const homeURL = (`${req.session.user_id}/restaurants`)
     res.redirect(homeURL);
   });
 
-//   if (validate) {
-//     req.session.user_id = validUser._id;
-//     console.log(req.session.user_id)
-//     return res.redirect(`/${req.session.user_id}/restaurants`);
-//   } else {
-//     console.log("no sir");
-//   }
-// });
 
-// app.post('/logout',(req, res)=>{
-//   req.session.destroy();
-//   res.redirect('login');
-// })
 app.get("/:id/restaurants", requireAuth, async (req, res) => {
     const id = req.session.user_id;
     const user = await User.findById(id);
@@ -119,11 +128,11 @@ app.get('/:id/restaurants/new', requireAuth, (req, res)=>{
     res.render('new', {id, cuisine, states})
 })
 
-// app.get('/:id', async(req,res)=>{
-//     const { id } = req.params;
-//     const restaurant = await Restaurant.findById(id)
-//     res.render('show', { restaurant })
-// })
+app.get('/:id', async(req,res)=>{
+    const { id } = req.params;
+    const restaurant = await Restaurant.findById(id)
+    res.render('show', { restaurant })
+})
 
 app.post('/:id/restaurants', requireAuth, async (req, res)=>{
     const id = req.session.user_id;
@@ -178,17 +187,9 @@ app.delete("/delete/:id", requireAuth, async(req,res)=>{
     req.session.user_id = null;
     res.redirect('/login');
   })
-
-//     await Restaurant.findByIdAndDelete(resId);
-//     const user = await User.findById(id);
-//     user.restaurants.pull(resId);
-    
-//     await user.save();
-//     res.redirect(`/${id}/restaurants`)
-// })
-// app.all('*',(req,res)=>{
-//   res.render('unknown');
-// })
+app.all('*',(req,res)=>{
+  res.render('unknown');
+})
 
 app.listen(8080, () => {
   console.log("yeyey");
